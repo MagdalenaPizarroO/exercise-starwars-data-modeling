502b22733e369d8484fd1f61922f8685828a4528
MagdalenaPizarroO <123502477+MagdalenaPizarroO@users.noreply.github.com>
2023-04-16 02:21:37 +0000

    mejores las relaciones entre tablas

--- src/models.py
+++ src/models.py
@@ -20,25 +20,30 @@ class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     planet_name = Column(String)
+    Favorites_Planets = relationship("Favorites_Planets", uselist=False)
 
 class People(Base):
     __tablename__ = 'people'
     id = Column(Integer, primary_key=True)
     person_name = Column(String)
+    favorite_people = relationship("Favorites_People", uselist=False)
 
 class Favorites_Planets(Base):
     __tablename__ = 'favorite_planets'
     fav_id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
-    Users = relationship("Users")
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    user = relationship("Users", backref="favorite_planets")
+    planet = relationship("Planets", uselist=False)
 
 class Favorites_People(Base):
     __tablename__ = 'favorite_people'
     fav_id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
-    Users = relationship("Users")
-
-
+    person_id = Column(Integer, ForeignKey('people.id'))
+    user = relationship("Users", backref="favorite_people")
+    person = relationship("People", uselist=False)
+    
     def to_dict(self):
         return {}
 
